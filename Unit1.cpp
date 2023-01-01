007cdd656ac16a4adeb5470da6ec12ea4d790a98
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2023-01-01 15:40:26 +0100

    Initial commit

---
+++ Unit1.cpp
@@ -0,0 +1,21 @@
+#include<math.h>
+#include<stdio.h>
+#include<conio.h>
+int p;
+void main()
+{
+        printf("entrer un nbr");
+        scanf("%d",&p);
+        switch(p)
+        {
+                case 1:printf("un");
+                break;
+                case 5:printf("cinq");
+                break;
+                case 8:printf("huit");
+                break;
+                default:printf("HAHAHAHA");
+        }
+        printf ("\n""VILOMA");
+getch();
+}
